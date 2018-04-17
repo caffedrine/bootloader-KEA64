aae5f43edc5c786122a02474d86a6fb47446db03
caffedrine <bacchusstyle@yahoo.com>
2018-04-17 14:09:56 +0300

    Sources organized!

--- Sources/Headers/derivative.h
+++ Sources/Headers/derivative.h
@@ -1,10 +1,24 @@
-/*
- * Note: This file is recreated by the project wizard whenever the MCU is
- *       changed and should not be edited by hand
- */
+// Device specific configurations
 
-/* Include the derivative-specific header file */
-#include <SKEAZN642.h>
-#include "typedefs.h"
+#define SKEAZ1284
+//#define SKEAZN642
 
 
+#ifdef SKEAZ1284
+	#include "SKEAZ1284.h"
+	// PINSEL translation
+	#define SIM_PINSEL	SIM_PINSEL_REG(SIM)
+
+	// Translations
+	#define FTMRH						FTMRE
+	#define FTMRH_FSTAT 				FTMRE_FSTAT
+	#define FTMRH_FSTAT_CCIF_MASK		FTMRE_FSTAT_CCIF_MASK
+	#define FTMRH_FCLKDIV				FTMRE_FCLKDIV
+	#define FTMRH_FCLKDIV_FDIVLCK_MASK	FTMRE_FCLKDIV_FDIVLCK_MASK
+	#define FTMRH_FCLKDIV_FDIVLD_MASK	FTMRE_FCLKDIV_FDIVLD_MASK
+	#define FTMRH_FCLKDIV_FDIV_MASK		FTMRE_FCLKDIV_FDIV_MASK
+	#define FTMRH_FCLKDIV_FDIV			FTMRE_FCLKDIV_FDIV
+
+#elif defined SKEAZN642
+	#include "SKEAZN642.h"
+#endif
